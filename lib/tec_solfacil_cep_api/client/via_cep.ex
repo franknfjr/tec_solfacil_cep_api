1a7f454eeaa2731547b7faf39ede5bd9241c9701
franknfjr <frankdonascimentojr@gmail.com>
2022-04-12 08:38:15 -0300

    feat: zip client

---
+++ lib/tec_solfacil_cep_api/client/via_cep.ex
@@ -0,0 +1,32 @@
+defmodule TecSolfacilCepApi.Client.ViaCep do
+  @base_url "https://viacep.com.br/ws/"
+  @tec_solfacil_cep_api :tec_solfacil_cep_api
+
+  @data ~w(cep logradouro complemento bairro localidade uf ibge ddd siafi)
+
+  def get_adress(zip) do
+    :get
+    |> Finch.build(@base_url <> "#{zip}" <> "/json/")
+    |> Finch.request(@tec_solfacil_cep_api)
+    |> handle_response
+  end
+
+  defp handle_response(request) do
+    case request do
+      {:ok, %{body: "{\n  \"erro\": true\n}", status: 200}} ->
+        {:error, :not_found}
+
+      {:ok, %{body: body, status: 200}} ->
+        {:ok, decoded_body} = Jason.decode(body)
+
+        result =
+          decoded_body
+          |> Map.take(@data)
+
+        {:ok, result}
+
+      _error ->
+        {:error, :invalid_zip}
+    end
+  end
+end
