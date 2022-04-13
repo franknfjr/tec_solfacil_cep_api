2968b60a2ce1c8778118b00891cca86852bdcda0
franknfjr <frankdonascimentojr@gmail.com>
2022-04-13 04:40:50 -0300

    feat: adding v1 route view and controller

--- lib/tec_solfacil_cep_api_web/router.ex
+++ lib/tec_solfacil_cep_api_web/router.ex
@@ -5,7 +5,11 @@ defmodule TecSolfacilCepApiWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api", TecSolfacilCepApiWeb do
+  scope "/api/v1", TecSolfacilCepApiWeb do
     pipe_through :api
+
+    get "/addresses", LocaleController, :index
+    get "/addresses/:cep", LocaleController, :show
+    post "/addresses", LocaleController, :create
   end
 end
---
+++ lib/tec_solfacil_cep_api_web/views/locale_view.ex
@@ -0,0 +1,26 @@
+defmodule TecSolfacilCepApiWeb.LocaleView do
+  use TecSolfacilCepApiWeb, :view
+  alias TecSolfacilCepApiWeb.LocaleView
+
+  def render("index.json", %{adresses: adresses}) do
+    %{data: render_many(adresses, LocaleView, "locale.json")}
+  end
+
+  def render("show.json", %{locale: locale}) do
+    %{data: render_one(locale, LocaleView, "locale.json")}
+  end
+
+  def render("locale.json", %{locale: locale}) do
+    %{
+      cep: locale.cep,
+      logradouro: locale.logradouro,
+      complemento: locale.complemento,
+      bairro: locale.bairro,
+      localidade: locale.localidade,
+      uf: locale.uf,
+      ibge: locale.ibge,
+      ddd: locale.ddd,
+      siafi: locale.siafi
+    }
+  end
+end
