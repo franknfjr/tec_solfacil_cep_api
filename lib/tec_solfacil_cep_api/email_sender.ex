75ded2b88d04fcadfb3440f85b6ed28042944f3c
franknfjr <frankdonascimentojr@gmail.com>
2022-04-14 09:04:26 -0300

    refactor: updating attachment file

--- lib/tec_solfacil_cep_api/email_sender.ex
+++ lib/tec_solfacil_cep_api/email_sender.ex
@@ -3,13 +3,19 @@ defmodule TecSolfacilCepApi.EmailSender do
   import Swoosh.Email
   alias TecSolfacilCepApi.Mailer
 
-  def send_email(email) do
+  def send_csv_to_email(file, email) do
     new()
     |> to({"noreply", "noreply@tecsolfacil.com"})
     |> from({email, email})
     |> subject("All csv's!")
     |> html_body("<h1>Hello #{email}!")
     |> text_body("Hello #{email}!")
+    |> attachment(
+      Swoosh.Attachment.new({:data, file},
+        filename: "tecsolfacil_addresses.csv",
+        content_type: "text/csv"
+      )
+    )
     |> Mailer.deliver()
   end
 end
