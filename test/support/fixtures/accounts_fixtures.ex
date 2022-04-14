871c797a529bccd6023c163ad62cfbe7bd7e1a30
franknfjr <frankdonascimentojr@gmail.com>
2022-04-14 18:16:21 -0300

    refactor: create functions to return some asserted data

--- test/support/fixtures/accounts_fixtures.ex
+++ test/support/fixtures/accounts_fixtures.ex
@@ -4,13 +4,24 @@ defmodule TecSolfacilCepApi.AccountsFixtures do
   entities via the `TecSolfacilCepApi.Accounts` context.
   """
 
+  def valid_user,
+    do: %{
+      email: "email@example.com",
+      password: "password"
+    }
+
+  def invalid_user,
+    do: %{
+      email: "foo@bar.com",
+      password: "fooo"
+    }
+
+  def valid_user_password, do: "password"
+
   def user_fixture(attrs \\ %{}) do
     {:ok, user} =
       attrs
-      |> Enum.into(%{
-        email: "email@example.com",
-        password: "password"
-      })
+      |> Enum.into(valid_user())
       |> TecSolfacilCepApi.Accounts.create_user()
 
     user
--- test/support/fixtures/localities_fixtures.ex
+++ test/support/fixtures/localities_fixtures.ex
@@ -4,22 +4,24 @@ defmodule TecSolfacilCepApi.LocalitiesFixtures do
   entities via the `TecSolfacilCepApi.Localities` context.
   """
 
-  @address_data %{
-    "bairro" => "Maguari",
-    "cep" => "67145037",
-    "complemento" => "(Cj PAAR)",
-    "ddd" => "91",
-    "ibge" => "1500800",
-    "localidade" => "Ananindeua",
-    "logradouro" => "Quadra Cem",
-    "siafi" => "0415",
-    "uf" => "PA"
-  }
+  def address_data do
+    %{
+      "bairro" => "Maguari",
+      "cep" => "67145037",
+      "complemento" => "(Cj PAAR)",
+      "ddd" => "91",
+      "ibge" => "1500800",
+      "localidade" => "Ananindeua",
+      "logradouro" => "Quadra Cem",
+      "siafi" => "0415",
+      "uf" => "PA"
+    }
+  end
 
   def locale_fixture(attrs \\ %{}) do
     {:ok, locale} =
       attrs
-      |> Enum.into(@address_data)
+      |> Enum.into(address_data())
       |> TecSolfacilCepApi.Localities.create_locale_address()
 
     locale
