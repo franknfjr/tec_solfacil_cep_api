a5ff8702263273eea869f6fdabefcb9309e1fa3b
franknfjr <frankdonascimentojr@gmail.com>
2022-04-14 09:05:26 -0300

    feat: csv worker

---
+++ lib/tec_solfacil_cep_api/workers/csv_worker.ex
@@ -0,0 +1,22 @@
+defmodule TecSolfacilCepApi.Workers.CSVWorker do
+  @moduledoc false
+  use Oban.Worker, queue: :file_csv
+  alias TecSolfacilCepApi.EmailSender
+  alias TecSolfacilCepApi.Repo
+  alias TecSolfacilCepApi.Entities.Localities.Locale
+
+  @impl Oban.Worker
+  def perform(%Oban.Job{args: %{"email" => email}}) do
+    csv = build_csv()
+
+    file = EmailSender.send_csv_to_email(csv, email)
+
+    {:ok, file}
+  end
+
+  defp build_csv do
+    Locale
+    |> Repo.all()
+    #implementing
+  end
+end
